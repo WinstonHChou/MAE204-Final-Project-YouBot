7b2b5e582da3413fa233a0d959d9f086497cca13
Winston <winston.h.chou@gmail.com>
2025-03-09 17:08:32 -0700

    [BugFix] `bound.m`

--- src/bound.m
+++ src/bound.m
@@ -7,5 +7,5 @@ function [x_bounded, in_bound] = bound(x, lower_bound, upper_bound)
 %         in_bound: if x within bounds
 
     x_bounded = max(lower_bound, min(x, upper_bound)); 
-    in_bound = (x >= lower_bound) && (x <= upper_bound);
+    in_bound = ((x >= lower_bound) + (x <= upper_bound)) == 2;
 end
