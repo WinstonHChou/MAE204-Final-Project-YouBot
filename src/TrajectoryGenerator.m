c6b7bcc0af3c3c5f1e87e587019a1039c3292d62
Winston <winston.h.chou@gmail.com>
2025-03-13 13:21:01 -0700

    [Refactor] restruct `TrajectoryGenerator.m`

--- src/TrajectoryGenerator.m
+++ src/TrajectoryGenerator.m
@@ -15,56 +15,43 @@ function [traj] = TrajectoryGenerator(T_se_initial, T_sc_initial, T_sc_final, T_
 %       - traj : A representation of the N configurations of the
 %                end-effector along the entire concatenated eight-segment
 %                reference trajectory. 
+    
+    addpath('external/ModernRobotics/packages/MATLAB/mr')
+    
+    % delete old csv file 
+    delete('trajectory.csv')
+    
+    N = k / 0.01;
+    
+    T_se_standoff_intial = T_sc_initial * T_ce_standoff;
+    
+    T_se_standoff_final = T_sc_final * T_ce_standoff;
+    T_se_standoff_final(3,4) = 0.15;
+    
+    T_se_grasp_final = T_se_standoff_final;
+    T_se_grasp_final(3,4) = 0.05;
 
-addpath('external/ModernRobotics/packages/MATLAB/mr')
-
-% delete old csv file 
-delete('trajectory.csv')
-
-N = k / 0.01;
-
-T_se_standoff_intial = T_sc_initial * T_ce_standoff;
-
-T_se_standoff_final = T_sc_final * T_ce_standoff;
-T_se_standoff_final(3,4) = 0.15;
-
-T_se_grasp_final = T_se_standoff_final;
-T_se_grasp_final(3,4) = 0.05;
-
-% Starting Position  --> Standoff over Cube
-traj1 = ScrewTrajectory(T_se_initial, T_se_standoff_intial, 3, N, 5);
-trajectoryToCsv(traj1, 0)
-
-% Standoff over Cube --> To End-Effector Grasp 
-traj2 = ScrewTrajectory(T_se_standoff_intial, T_ce_grasp, 1, N, 5);
-trajectoryToCsv(traj2, 0)
-
-% End-Effector Grasp --> End-Effector Grasp 0.625
-traj3 = ScrewTrajectory(T_ce_grasp, T_ce_grasp, 1, N, 5);
-trajectoryToCsv(traj3, 1)
-
-% End-Effector Grasp --> Standoff over Cube
-traj4 = ScrewTrajectory(T_ce_grasp, T_se_standoff_intial, 2, N, 5);
-trajectoryToCsv(traj4, 1)
-
-% Standoff over Cube Initial --> Standoff over Cube Final
-traj5 = ScrewTrajectory(T_se_standoff_intial, T_se_standoff_final, 3, N, 5);
-trajectoryToCsv(traj5, 1)
-
-% Standoff over Cube Final --> End-Effector Grasp 
-traj6 = ScrewTrajectory(T_se_standoff_final, T_se_grasp_final, 2, N, 5);
-trajectoryToCsv(traj6, 1)
-
-% End-Effector Grasp --> End-Effector Grasp 0.625
-traj7 = ScrewTrajectory(T_se_grasp_final, T_se_grasp_final, 1, N, 5);
-trajectoryToCsv(traj7, 0)
-
-% End-Effector Grasp --> End-Effector Standoff
-traj8 = ScrewTrajectory(T_se_grasp_final, T_se_standoff_final, 1, N, 5);
-trajectoryToCsv(traj8, 0)
-
-traj = [traj1, traj2, traj3, traj4, traj5, traj6, traj7, traj8];
-
+    %% Initialize trajectories
+    % Starting Position  --> Standoff over Cube
+    % Standoff over Cube --> To End-Effector Grasp
+    % End-Effector Grasp --> End-Effector Grasp 0.625
+    % End-Effector Grasp --> Standoff over Cube
+    % Standoff over Cube Initial --> Standoff over Cube Final
+    % Standoff over Cube Final --> End-Effector Grasp
+    % End-Effector Grasp --> End-Effector Grasp 0.625
+    % End-Effector Grasp --> End-Effector Standoff
+    
+    traj = cell(1,8*N);
+    X = {T_se_initial, T_se_standoff_intial, T_ce_grasp, T_ce_grasp, T_se_standoff_intial, T_se_standoff_final, T_se_grasp_final, T_se_grasp_final, T_se_standoff_final};
+    Xstarts = X(1:end-1);
+    Xends = X(2:end);
+    desired_durations = [3, 1, 1, 2, 3, 2, 1, 1];
+    grasp_states = [0, 0, 1, 1, 1, 1, 0, 0];
+    
+    for i = [1:length(X)-1]
+        traj(1,1+N*(i-1):N*i) = ScrewTrajectory(Xstarts{i}, Xends{i}, desired_durations(i), N, 5);
+        trajectoryToCsv(traj(1,1+N*(i-1):N*i), grasp_states(i))
+    end
 end
 
 function trajectoryToCsv(traj, gripperState)
