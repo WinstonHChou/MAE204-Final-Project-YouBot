692beb9c8e6f5df5d1459f64f705ea3ed8690138
Winston <winston.h.chou@gmail.com>
2025-03-13 01:37:48 -0700

    WIP: `FeedbackControl`, calculate current `T_se` based on given `q`

--- src/FeedbackControl.m
+++ src/FeedbackControl.m
@@ -1,7 +1,7 @@
-function [endEffectorTwist_e, wheelSpeeds, jointSpeeds] = FeedbackControl(T_se, T_se_d, T_se_d_next, Kp, Ki, dt)
+function [endEffectorTwist_e, wheelSpeeds, jointSpeeds] = FeedbackControl(q, T_se_d, T_se_d_next, Kp, Ki, dt)
 %FEEDBACKCONTROL Summary of this function goes here
 %   Inputs: 
-%   - T_se        : The current actual end-effector configuration 
+%   - q           : The current state vector (12 variables)
 %   - T_se_d      : The current reference end-effector configuration 
 %   - T_se_d_next : The reference end-effector configuration at the next timestep
 %   - Kp          : P gain matrix
@@ -13,25 +13,52 @@ function [endEffectorTwist_e, wheelSpeeds, jointSpeeds] = FeedbackControl(T_se, 
 %   - wheelSpeeds        : Commanded wheel speeds (u)
 %   - jointSpeeds        : Commanded arm joint speeds (theta_dot)
 
-% Running error 
-persistent errorControl
+    % Running error 
+    persistent errorControl
+    load("youBotParams.mat")
+    
+    %% Base Body Jacobian
+    % Compute Current T_sb (chassis (Body) frame to odom (Space) frame)
+    T_sb = [[cos(q(1)), -sin(q(1)), 0,  q(2)];
+            [sin(q(1)),  cos(q(1)), 0,  q(3)];
+            [       0,           0, 1,    z0];
+            [       0,           0, 0,     1]];
+    % Compute Current T_0e (E-E frame to Arm Base frame)
+    T_0e = FKinBody(M_0e, B, q(4:8));
+    % Compute Current T_se (E-E frame to odom (Space) frame)
+    T_se = T_sb * T_b0 * T_0e;
+    % Compute Chassis Velocity Kinematics in 6D
+    F6 = [zeros(1,4);
+          zeros(1,4);
+                   F;
+          zeros(1,4)];
+    % Compute Jbase relative to E-E frame
+    Jbase = Adjoint(TransInv(T_0e) * TransInv(T_b0)) * F6;
 
-% Base Jacobian 
+    %% Arm Body Jacobian 
+    % Check if joints are within arbitrary limits
+    [empty, joints_checked] = checkJointLimits(q);
+    % Compute Jarm based on given state vector, q
+    Jarm = JacobianBody(B, q(4:8));
+    % Ignore Jacobian of the joints who reaches the joint limits
+    Jarm(:, ~joints_checked) = 0;
+    
+    %% youBot Body Jacobain
+    Jb = [Jarm Jbase]; % order: q_dot = [theta_dot; wheel_speeds]
+    % disp(Jb)
 
-% Arm Jacobian 
-Jarm = JacobianBody(Blist, )
-
-% Xerr 
-Xerr = log(inv(T_se)*T_se_d);
-
-% Feedforward reference twist
-refTwist = (1/dt)*log(inv(T_se_d) * T_se_d_next);
-
-% Feedforward plus feedback control law (equation 13.37)
-% twist = 
-
-% Accumulate error every timestep
-errorControl = Xerr * dt;
+    %% Control
+    % Xerr 
+    Xerr = log(inv(T_se)*T_se_d);
+    
+    % Feedforward reference twist
+    refTwist = (1/dt)*log(inv(T_se_d) * T_se_d_next);
+    
+    % Feedforward plus feedback control law (equation 13.37)
+    % twist = 
+    
+    % Accumulate error every timestep
+    errorControl = Xerr * dt;
 
 end
 
--- src/test_FeedbackControl.m
+++ src/test_FeedbackControl.m
@@ -1,27 +1,39 @@
 clc; clear; close all;
 
+addpath('external/ModernRobotics/packages/MATLAB/mr')
 %% Test inputs provided in final project instructions 
+load("youBotParams.mat")
 
 % Robot Configuration
 robotConfig = [0, 0, 0, 0, 0, 0.2, -1.6, 0];
+q = [robotConfig, 0, 0, 0, 0]'; % wheels pos doesn't matter
 
-% Computed from robotConfig given above
-X = [[0.170, 0.0, 0.985, 0.387];
-     [0, 1, 0, 0];
+% Computed from state vector, q given above
+T_sb = [[cos(q(1)), -sin(q(1)), 0,  q(2)];
+        [sin(q(1)),  cos(q(1)), 0,  q(3)];
+        [       0,           0, 1,    z0];
+        [       0,           0, 0,     1]];
+X_0e = FKinBody(M_0e, B, robotConfig(4:8)');
+X_se = T_sb * T_b0 * X_0e;
+
+% Verify X_se matches X
+X = [[ 0.170, 0, 0.985, 0.387];
+     [     0, 1,     0,     0];
      [-0.985, 0, 0.170, 0.570];
-     [0, 0, 0, 1]];
+     [     0, 0,     0,     1]];
+fprintf("Errors between X and X_se: \n%f\n", sum(sum(X_se - X)))
 
 % Xd 
-Xd = [[0, 0, 1, 0.5] ;
-      [0, 1, 0, 0]   ; 
+Xd = [[ 0, 0, 1, 0.5];
+      [ 0, 1, 0,   0]; 
       [-1, 0, 0, 0.5];
-      [0, 0, 0, 1]];
+      [ 0, 0, 0,   1]];
 
 % Xd,next
-Xd_next = [[0, 0, 1, 0.6] ;
-           [0, 1, 0, 0]   ; 
+Xd_next = [[ 0, 0, 1, 0.6];
+           [ 0, 1, 0,   0]; 
            [-1, 0, 0, 0.3];
-           [0, 0, 0, 1]  ];
+           [ 0, 0, 0,   1]];
 
 % dt 
 dt = 0.01;
@@ -30,4 +42,4 @@ dt = 0.01;
 Kp = zeros(4);
 Ki = zeros(4);
 
-
+[endEffectorTwist_e, wheelSpeeds, jointSpeeds] = FeedbackControl(q, Xd, Xd_next, Kp, Ki, dt)
